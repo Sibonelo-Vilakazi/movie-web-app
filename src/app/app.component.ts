7d21d692f243bbe1ea924b54f1b8c398aff57c5e
Sibonelo Vilakazi <sibonelovilakazi@Sibonelos-MacBook-Pro.local>
2024-08-04 15:16:03 +0200

    starter

---
+++ src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet, NgbModule],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss'
+})
+export class AppComponent {
+  title = 'movie-web-app';
+}
